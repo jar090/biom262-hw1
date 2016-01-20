8b14a2d935b58f12bf8a16bd43fd0c3b0d9337d9
Julie Roberts <jar090@ucsd.edu>
2016-01-19 18:39:21 -0800

    added echo line

--- tf_binding.sh
+++ tf_binding.sh
@@ -31,3 +31,25 @@ echo '--- Random 10 lines ---'
 awk -v seed=907 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}' gencode.v19.annotation.chr22.transcript.promoter.gtf | head
 echo '--- Last 10 lines ---'
 tail gencode.v19.annotation.chr22.transcript.promoter.gtf
+
+module load biotools
+bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed  > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+wc -l gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+echo '--- First 10 lines ---'
+head gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+echo '--- Random 10 lines ---'
+awk -v seed=908 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}' gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf | head
+echo '--- Last 10 lines ---'
+tail gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+
+module load biotools
+bedtools getfasta -s -fi ~/code/biom262-2016/weeks/week01/data/GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
+wc -l gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
+echo '--- First 10 lines ---'
+head gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
+echo '--- Random 10 lines ---'
+awk -v seed=908 'BEGIN{srand(seed);}{ if (rand() < 0.5 ) {print $0}}' gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta | head
+echo '--- Last 10 lines ---'
+tail gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta
+
+echo "Hello I am a message in standard error (stderr)" >&2
